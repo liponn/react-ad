6f10d89ede10df313232129f44e4e152ef127d7a
Administrator <lizhenhua@wanglibank.com>
2016-06-23 11:06:31 +0800

    文章管理

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
-import { commonFetch } from '../../actions/omg';
+import { commonFetch ,fetchAction} from '../../actions/omg';
 import { Link, Card } from '../tools';
 import { showModal, hideModal } from '../../actions/modal';
 import ArticleAddModal from '../modals/ArticleAddModal';
@@ -17,13 +17,21 @@ class Article extends Component {
     this.showPutModal = this.showPutModal.bind(this);
     this.upArticle = this.upArticle.bind(this);
     this.downArticle = this.downArticle.bind(this);
+    this.articleByType = this.articleByType.bind(this);
   }
   componentDidMount() {
-    this.props.dispatch(commonFetch(ARTICLE_TYPE_LIST,'GET',false,'/0'));
-    this.props.dispatch(commonFetch(ARTICLE_LIST,'GET', false, '/3/10'));
+    this.props.dispatch(fetchAction({type:ARTICLE_TYPE_LIST,method:'GET',suffix:'/0',key:'0'}))
+      .then((json)=>{
+        const type_id=json.data[0].id;
+        $("#articleAdd").attr("data-id",type_id);
+        $("#btnType button").eq(0).addClass('focus');
+        this.props.dispatch(fetchAction({type:ARTICLE_TYPE_LIST,method:'GET',suffix:'/'+type_id,key:'subType'}));
+        this.props.dispatch(fetchAction({type:ARTICLE_LIST,method:'GET',suffix: '/'+type_id+'/10',key:'articleList'}));
+      });
   }
-  showModal() {
-    const  modalView = <ArticleAddModal />;
+  showModal(e) {
+    const id = $(e.target).data('id');
+    const  modalView = <ArticleAddModal  type_id={id}/>;
     this.props.dispatch(showModal(modalView));
   }
   showPutModal(e){
@@ -66,14 +74,36 @@ class Article extends Component {
     this.props.dispatch(commonFetch(ARTICLE_DOWN, 'GET',false ,"/"+id))
       .then(() => (this.props.dispatch(commonFetch(ARTICLE_LIST))));
   }
+  articleByType(e){
+    const self = $(e.target);
+    const id = self.data('id');
+    $("#articleAdd").attr("data-id",id);
+    self.addClass('focus').siblings().removeClass('focus');
+    self.parent().siblings().find('button').removeClass('focus');
+    if(self.parent().attr('id')=='btnType'){
+      this.props.dispatch(fetchAction({type:ARTICLE_TYPE_LIST,method:'GET',suffix:'/'+id,key:'subType'}))
+    }
+    /*this.props.dispatch(commonFetch(ARTICLE_LIST,'GET', false, '/'+id+'/10'))
+      .then((json)=>{
+        this.props.items = json.data;
+      })*/
+    this.props.dispatch(fetchAction({type:ARTICLE_LIST,method:'GET',suffix: '/'+id+'/10',key:'articleList'}));
+  }
   render() {
-    const items = this.props.items;
-    const typeItems = this.props.typeItems;
+    const items = this.props.items["articleList"]?this.props.items["articleList"].data:[];
+    const typeItems = this.props.typeItems||{};
+    const fatherItems =typeItems[0]||[];
+    const subtypeItems = typeItems['subType']||[];
     return (
       <div>
         <div id="btnType" class="btn-group" role="group" style={{marginBottom:"20px"}}>
-          {typeItems.map((item) => (
-          <button type="button" data-id={item.id} className="btn btn-primary-outline">{item.name}</button>
+          {fatherItems.map((item,index) => (
+            <button type="button" data-id={item.id} className="btn btn-primary-outline" onClick={this.articleByType}>{item.name}</button>
+          ))}
+        </div>
+        <div id="btnSubType" class="btn-group" role="group" style={{marginBottom:"20px"}}>
+          {subtypeItems.map((item) => (
+            <button type="button" data-id={item.id} className="btn btn-primary-outline btn-sm" onClick={this.articleByType}>{item.name}</button>
           ))}
         </div>
         <div className="card">
@@ -84,7 +114,7 @@ class Article extends Component {
               data-toggle="modal"
               data-target="#channel-add-modal"
             >
-              <i className="fa fa-plus" data-toggle="modal" data-target="#cahnnel-add-modal" onClick={this.showModal}> 添加</i>
+              <i id="articleAdd" className="fa fa-plus" data-toggle="modal" data-target="#cahnnel-add-modal" data-id="" onClick={this.showModal}> 添加</i>
             </button>
             <Link
               className="btn btn-sm  btn-info pull-right"
@@ -138,17 +168,17 @@ Article.propTypes = {
   dispatch: PropTypes.func.isRequired
 };
 Article.defaultProps = {
-  items: [],
-  typeItems:[]
-}
+  items: {},
+  typeItems:{}
+};
 
 export default connect(state => {
   const { omg } = state;
-  const { data } = omg[ARTICLE_LIST] || [];
-  const typeItems = omg[ARTICLE_TYPE_LIST] || [];
+  const items = omg[ARTICLE_LIST] || [];
+  const typeItems = omg[ARTICLE_TYPE_LIST] || {};
   return {
-    items: data,
-    typeItems
+    items,
+    typeItems,
   };
 })(Article);
 
