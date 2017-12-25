e925b5d093c563607a0eb98b2dbb8549edf81558
zhaodongran <zhaodongran@wanglibank.com>
2017-12-25 17:50:54 +0800

    调试

--- src/components/Pages/Thread/Thread.js
+++ src/components/Pages/Thread/Thread.js
@@ -4,10 +4,8 @@ import { connect } from 'react-redux';
 import { commonFetch, fetchAction } from '../../../actions/omg';
 import { showModal, hideModal } from '../../../actions/modal';
 import { BBS_THREAD_LIST,BBS_SECTION_LIST } from '../../../constants';
-import { Radio,Pagination,DateTimeInput } from '../../tools';
+import { Radio,Pagination } from '../../tools';
 import { DatePicker,Select,Form,Input } from "antd";
-import ActivityAddModal from '../../modals/ActivityAddModal';
-import { getConfig } from '../../../config/omg';
 
 class Thread extends Component {
     constructor(props) {
@@ -18,10 +16,10 @@ class Thread extends Component {
         this.changeLength = this.changeLength.bind(this);
         this.changePage = this.changePage.bind(this);
         this.changeSearch= this.changeSearch.bind(this);
-        //this.freshThreadList = this.freshThreadList.bind(this);
+        //this.componentDidMount = this.componentDidMount.bind(this);
         //this.updateThread = this.updateThread.bind(this);
         this.state = {
-            isVerify:0
+            isVerify:0,
         }
     }
     componentDidMount() {
@@ -31,8 +29,8 @@ class Thread extends Component {
 
     list(){
         const queryObj = {};
-        const isVerify = this.state.isVerify || 0;
         const page = this.state.page || 1;
+        const isVerify = this.state.isVerify || 0;
         const pageNum = this.state.pageNum || 20;
         const searchParams = this.state.formData || [];
         queryObj[`page`] = page;
@@ -45,13 +43,11 @@ class Thread extends Component {
         }
         if(searchParams.created_at){
             queryObj[`data[filter][created_at]`] = searchParams.created_at.format('YYYY-MM-DD');
-            queryObj[`data[filter][created_at_pattern]`] = "max_equal";
+            queryObj[`data[filter][created_at_pattern]`] = "min_equal_max";
         }
         if(searchParams.end_at){
-            queryObj[`data[filter][created_at_end]`] = searchParams.created_at.format('YYYY-MM-DD');
-            queryObj[`data[filter][created_at_end_pattern]`] = "min_equal";
+            queryObj[`data[filter][end_at]`] = searchParams.end_at.format('YYYY-MM-DD');
         }
-        console.log(queryObj,"queryObj");
         const requert = {
             type:BBS_THREAD_LIST,
             key:isVerify,
@@ -61,25 +57,27 @@ class Thread extends Component {
     }
 
     typeChange(e){
-        const value = e.target.value;
+        const isVerify = e.target.value;
         this.setState({
-                isVerify:value,
-                page:1,
-            },() => {this.list()}
-        );
+            isVerify,
+            page:1,
+        });
+        this.list();
     }
 
     changeLength(e){
         const length = e.target.value;
         this.setState({
             pageNum:length
-        },() => {this.list()});
+        });
+        this.list();
     }
 
     changePage(page){
         this.setState({
             page:page
-        },()=>{this.list()});
+        });
+        this.list()
     }
 
     changeSearch(e){
@@ -87,8 +85,10 @@ class Thread extends Component {
         const  formData = this.props.form.getFieldsValue();
         this.setState({
             formData:formData,
-        },()=>{this.list()});
+        });
+        this.list()
     }
+
     freshSectionList() {
         this.props.dispatch(fetchAction({
             type: BBS_SECTION_LIST,
@@ -96,39 +96,13 @@ class Thread extends Component {
         }));
     }
 
-    // 更新活动
-    /* updateActivity(e) {
-     e.preventDefault();
-     const formData = new FormData(e.target);
-     this.props.dispatch(fetchAction({
-     type: ACTIVITY_PUT,
-     method: 'POST',
-     formData,
-     })).then(json => {
-     if (json.error_code === 0) {
-     this.props.dispatch(hideModal(true));
-     this.freshActivityInfo();
-     }
-     });
-     }
-     // 更新活动
-     showUpdateActivity() {
-     if (!this.activity || !this.activity.id) {
-     alert('获取活动详情失败');
-     return;
-     }
-     this.props.dispatch(showModal(<ActivityAddModal item={this.activity} update submit={this.updateActivity} />));
-     }*/
-
-
     render() {
         const threadList = this.props.threadList[this.state.isVerify] || {};
         const sectionList = this.props.sectionList || {};
         const sections = sectionList.type || [];
-        const items = threadList.data;
-        const Option  = Select.Option;
-        const FormItem = Form.Item;
+        const items = threadList.data || [];
         const { getFieldDecorator,getFieldsValue } = this.props.form;
+
         const updateActivityBtn = (
             <button
                 /*onClick={this.showUpdateActivity}*/
@@ -137,20 +111,19 @@ class Thread extends Component {
                 <i className="fa fa-edit">编辑</i>
             </button>
         );
-
         return (
             <div>
                 <Radio
                     labelName="未审核"
                     name="userfilter"
-                    checked={items && items[0].isverify === 0}
+                    checked={this.state.isVerify ===  0}
                     onChange={this.typeChange}
                     value={0}
                 />
                 <Radio
                     labelName="已审核"
                     name="userfilter"
-                    checked={items && items[0].isverify === 1}
+                    checked={this.state.isVerify === 1}
                     onChange={this.typeChange}
                     value={1}
                 />
@@ -158,13 +131,13 @@ class Thread extends Component {
                     labelName="已拒绝"
                     name="userfilter"
                     value={2}
-                    checked={items && items[0].isverify === 2}
+                    checked={this.state.isVerify === 2}
                     onChange={this.typeChange}
                 />
                 <hr />
                 <div>
-                    <div className="card clearfix">
-                        <div className="clearfix">
+                    <div className="card clearfix ">
+                        <div className="clearfix m-t-1">
                             <h4 className="card-title">
                                 <div className="pull-left m-l-1">
                                     帖子管理
@@ -187,36 +160,33 @@ class Thread extends Component {
                                         unurl
                                     />
                                 </div>
-                            </h4>
-                        </div>
-                        <div className="clearfix">
-                            <h4 className="card-title">
-                                <Form className="form-inline pull-left" onSubmit={this.changeSearch}>
+
+                                <Form className="form-inline pull-right" onSubmit={this.changeSearch}>
                                     <div className="pull-left m-l-1 m-b-1">
-                                            {getFieldDecorator('type_id', {
-                                                rules: [],
-                                            })(
-                                                <Select style={{ width: 120 }}>
-                                                    {sections.map((item,index) => (
-                                                        <Option key={index} value={item.id}>{item.name}</Option>
-                                                    ))}
-                                                </Select>
-                                            )}
-                                            &nbsp;
-                                            {getFieldDecorator('created_at',
-                                                {rules:[]},
-                                            )(
-                                                <DatePicker placeholder="START"/>
-                                            )}
-
-
-                                            {getFieldDecorator("end_at",
-                                                {rules:[]},
-                                            )(
-                                                <DatePicker placeholder="END"/>
-                                            )}
-                                    </div>
+                                        {getFieldDecorator('type_id', {
+                                            rules: [],
+                                        })(
+                                            <Select style={{ width: 120 }}>
+                                                {sections.map((item,index) => (
+                                                    <Option key={index} value={item.id}>{item.name}</Option>
+                                                ))}
+                                            </Select>
+                                        )}
                                         &nbsp;
+                                        {getFieldDecorator('created_at',
+                                            {rules:[]},
+                                        )(
+                                            <DatePicker placeholder="START"/>
+                                        )}
+
+
+                                        {getFieldDecorator("end_at",
+                                            {rules:[]},
+                                        )(
+                                            <DatePicker placeholder="END"/>
+                                        )}
+                                    </div>
+                                    &nbsp;
                                     <button type="submit"
                                             className="btn btn-sm btn-info-outline pull-right"
                                     >
@@ -228,22 +198,40 @@ class Thread extends Component {
                         <table className="table table-bordered m-b-0 table-hover data-table">
                             <thead>
                             <tr>
-                                <th>字段名</th>
+                                <th>ID</th>
+                                <th>所属版块</th>
+                                <th>贴子标题</th>
+                                <th>贴子内容</th>
+                                <th>用户ID</th>
+                                <th>用户昵称</th>
                                 <th>操作</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>字段值</td>
-                                <td>
-                                    <button
-                                        className="btn btn-info-outline btn-sm"
-                                    >编辑</button>
-                                    <button
-                                        className="btn btn-danger-outline btn-sm"
-                                    >删除</button>
-                                </td>
-                            </tr>
+                            { items.map((item,index) => (
+                                <tr key={`group-${index}`}>
+                                    <td>{item.id}</td>
+                                    <td>{item.section.name}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.content}</td>
+                                    <td>{item.user_id}</td>
+                                    <td>{item.user.nickname}</td>
+                                    <td>
+                                        <button data-id={item.id} onClick={this.updateThread}
+                                                className="btn btn-info-outline btn-sm"
+                                        >编辑</button>
+                                        <button data-id={item.id} onClick={this.updateThread}
+                                                className="btn btn-info-outline btn-sm"
+                                        >编辑</button>
+                                        <button data-id={item.id} onClick={this.updateThread}
+                                                className="btn btn-info-outline btn-sm"
+                                        >编辑</button>
+                                        <button data-id={item.id} onClick={this.delThread}
+                                                className="btn btn-danger-outline btn-sm"
+                                        >删除</button>
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                         <div className="card-block clearfix">
--- src/routes/thread/index.js
+++ src/routes/thread/index.js
@@ -3,10 +3,24 @@ import Thread from '../../components/pages/Thread';
 
 export default {
 
-  path: '/thread',
+    path: '/thread',
 
-  action({ context }) {
-    context.setTitle('运营后台 | 社区管理');
-    return <Thread />;
-  },
+    action() {
+        return <Thread />;
+    },
+    /*path: '/Thread',
+    children: [
+        {
+            path: '/:typeId',
+            action(context) {
+                const typeId = +context.params.isVerify;
+                return <Thread typeId={typeId} />;
+            },
+        },
+    ],
+    async action({ next, context }) {
+        context.setTitle('运营后台 | 活动管理');
+        const component = await next();
+        return component;
+    },*/
 };
